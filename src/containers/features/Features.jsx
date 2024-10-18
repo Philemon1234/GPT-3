009708555b52567b6eab3fe3c1ded1a993703dbf
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2024-10-18 07:13:44 +0300

    Completed WhatGPT section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -5,11 +5,11 @@ const Features = ({title, text}) => {
   return (
     <div className="gpt3__features-container__feature">
       <div className="gpt3__features-container__feature-title">
-        {/* <div /> */}
+        <div />
         <h1>{title}</h1>
       </div>
       <div className="gpt3__features-container__feature-text">
-        {text}
+        <p>{text}</p>
       </div>
     </div>
   )
